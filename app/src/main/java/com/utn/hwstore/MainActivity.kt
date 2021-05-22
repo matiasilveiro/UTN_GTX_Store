6a72b562ddceff8339179f5b3460636a4ac27ac0
Matias Silveiro <matias.silveiro@gmail.com>
2021-05-22 20:39:20 -0300

    NavigationDrawer funcionando

--- app/src/main/java/com/utn/hwstore/MainActivity.kt
+++ app/src/main/java/com/utn/hwstore/MainActivity.kt
@@ -6,64 +6,44 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
-import android.view.View
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
 import androidx.core.view.GravityCompat
-import androidx.core.view.get
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.NavigationUI
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.messaging.FirebaseMessaging
+import com.utn.hwstore.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.navdrawer_header.*
 
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener,
-    DrawerLayout.DrawerListener {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
-    private lateinit var drawerLayout: DrawerLayout
-    private lateinit var auth: FirebaseAuth
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
     private lateinit var messageReceiver: BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-
-        val navController = findNavController(R.id.nav_host_fragment)
-
-        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar,
-            R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        //val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
-
-        val navigationView = findViewById<NavigationView>(R.id.nav_view)
-        //navigationView.setupWithNavController(navController)
-        navigationView.setNavigationItemSelectedListener(this)
-
-        drawerLayout.addDrawerListener(this)
+        setupNavigation()
 
         setupNotificationsReceiver()
         LocalBroadcastManager.getInstance(this).registerReceiver(messageReceiver, IntentFilter("MyData"))
 
-        auth = FirebaseAuth.getInstance()
         val user = auth.currentUser
 
         user?.let {
@@ -75,7 +55,7 @@ class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
                 subscribeToNotificationTopic("notebooks", false)
             }
 
-            val header = navigationView.getHeaderView(0)
+            val header = binding.navigationView.getHeaderView(0)
             val headerTitle = header.findViewById<TextView>(R.id.txt_title)
             headerTitle.text = user.displayName
         }
@@ -91,39 +71,67 @@ class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
         LocalBroadcastManager.getInstance(this).unregisterReceiver(messageReceiver)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+    }
+
     override fun onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START)
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
         } else {
             super.onBackPressed()
         }
     }
 
-    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
-        Toast.makeText(this, "onNavigationItemSelected", Toast.LENGTH_SHORT).show()
-        when(menuItem.itemId) {
-            else -> Toast.makeText(this, "Not yet implemented. ID: ${menuItem.itemId}", Toast.LENGTH_SHORT).show()
-        }
 
-        drawerLayout.closeDrawer(GravityCompat.START)
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        item.isChecked = true
+
+        binding.drawerLayout.closeDrawers()
+
+        try {
+            when(item.itemId) {
+                R.id.nav_user_logout -> { logoutDialog() }
+                else -> { navController.navigate(item.itemId) }
+            }
+        } catch (e: Exception) {
+            Log.e("MainActivity", "Navigation Drawer exception $e")
+        }
 
         return true
     }
 
-    override fun onDrawerStateChanged(newState: Int) {
-        //TODO("Not yet implemented")
+    private fun logoutDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Cerrar sesión")
+            .setMessage("¿Estás seguro que deseas cerrar sesión?")
+            .setPositiveButton("Aceptar") { dialog: DialogInterface, which: Int ->
+                auth.signOut()
+                finish()
+            }
+            .setNegativeButton("Cancelar") { dialog: DialogInterface, which: Int ->
+                dialog.dismiss()
+            }
+            .show()
     }
 
-    override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
-        //TODO("Not yet implemented")
-    }
+    private fun setupNavigation() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
 
-    override fun onDrawerClosed(drawerView: View) {
-        //TODO("Not yet implemented")
-    }
+        val topLevelDestinations = setOf(
+            R.id.HWListFragment,
+        )
 
-    override fun onDrawerOpened(drawerView: View) {
-        //TODO("Not yet implemented")
+        appBarConfiguration = AppBarConfiguration.Builder(topLevelDestinations)
+            .setDrawerLayout(binding.drawerLayout)
+            .build()
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+        NavigationUI.setupWithNavController(binding.navigationView, navController)
+        binding.navigationView.setNavigationItemSelectedListener(this)
     }
 
     private fun setupNotificationsReceiver() {
